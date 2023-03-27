6296e6f1e4d0023064aab13d0d4e5b2a81655318
Luopengbo1 <51694640+Luopengbo1@users.noreply.github.com>
2023-03-27 21:45:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '即悦创投', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '即悦心理，悦享生活🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.jyctai.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '即悦创投, 心理服务', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '即悦创投,心理服务', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
